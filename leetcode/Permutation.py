5a71535f4d83fe5374ab5bc743fc16e0a1fe25f3
xulleon <leonxu@yahoo.com>
2022-08-12 02:17:59 -0700

    rename with title

--- leetcode/Permutation.py
+++ leetcode/Permutation.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/permutations/
+# Runtime: 40 ms, faster than 95.32% of Python3 online submissions for Permutations.
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         results = []
@@ -10,7 +12,10 @@ class Solution:
             results.append(subsets)
 
         for i in range(len(nums)):
+            # permutation is directional connection. Therefore, [1,2] and [2,1] are different.
+            # i always starts from 0. Combination is different, it will start from index to len(nums) - 1
             if visited[i]:
+                # visited is used to prevent a number being used multiple times.
                 continue
 
             subsets.append(nums[i])
