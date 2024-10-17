8aedfc7602c73477bfb86ecb4e62ac44e101b83e
xulleon <42684153+xulleon@users.noreply.github.com>
2024-10-17 15:54:38 -0700

    Update Binary_Tree_Level_Order_Traversal.py with a new version

--- leetcode/Chapter4_BFS/Binary_Tree_Traversal/Binary_Tree_Level_Order_Traversal.py
+++ leetcode/Chapter4_BFS/Binary_Tree_Traversal/Binary_Tree_Level_Order_Traversal.py
@@ -28,3 +28,35 @@ class Solution:
 
         return res
 
+# Another version
+# Runtime: 3 ms, faster than 100.00% of Python3 online submissions for Binary Tree Level Order Traversal.
+# Memory Usage: 17.3 MB, less than 15.21% of Python3 online submissions for Binary Tree Level Order Traversal.
+from collections import defaultdict, deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        
+        queue = deque([root])
+        visited = defaultdict(bool)
+        visited[root] = True
+        res = []
+        while queue:
+            level = []
+            size = len(queue)
+            for _ in range(size):
+                head = queue.popleft()
+                level.append(head.val)
+                for child in [head.left, head.right]:
+                    if child is not None and not visited[child]:
+                        queue.append(child)
+                        visited[child] = True
+            res.append(level)
+                        
+        return res
