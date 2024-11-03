b8dc98e398d209b18df6c05c63f0c6e300d7486e
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-02 23:03:24 -0700

    Update strStr_ii.py

--- leetcode/Chapter1_Hack_the_Algorithm/strStr_ii.py
+++ leetcode/Chapter1_Hack_the_Algorithm/strStr_ii.py
@@ -14,40 +14,38 @@ class Solution:
     @return: An integer as index
     """
     def str_str2(self, source: str, target: str) -> int:
-        # write your code here
         n, m = len(source), len(target)
         if m == 0:
             return 0
 
-        base = 1000000
-        power = 1
+        power = 1000000
+        base = 1
         for i in range(m):
-            power = (power * 31) % base
-        
+            base = (base * 31) % power
 
-        source_hash, target_hash = 0, 0
-        # calculate has for the target "bcd"
+        src_hash, target_hash = 0, 0
+        # calculate target hashes
         for i in range(m):
-            target_hash = (target_hash * 31 + ord(target[i])) % base
+            target_hash = (target_hash * 31 + ord(target[i])) % power
 
+        # calculate source hash
         for i in range(n):
-            source_hash = (source_hash * 31 + ord(source[i]) ) % base
-
+            src_hash = (src_hash * 31 + ord(source[i])) % power
+            #abc
             if i < m - 1:
                 continue
 
-            # abc -a
+            # abcd -> remove a
             if i >= m:
-                source_hash = source_hash - (ord(source[i - m]) * power) % base
-                if source_hash < 0:
-                    source_hash += base
+                src_hash = (src_hash - (ord(source[i - m]) * base) % power) % power
+                # src_has could be negative
+                if src_hash < 0:
+                    src_hash = (power + src_hash) % power
 
-            # abcd
-            if source_hash == target_hash:
-                # if hash is equal, then double check for each char
-                # if hashes are not equal, the two strings will definitely not be the same
-                # if the hashes are equal, the two strings may not be the same
-                if source[i-m+1: i + 1] == target:
+            if src_hash == target_hash:
+                # abc. now i = 2 points to c. m is 3, therefore, the first 
+                # char is at 0, therefore, i - m + 1 = 2 -3 + 1 = 0.
+                # The range is to i, therefore, we need to use i + 1
+                if source[i -m + 1: i + 1] == target:
                     return i - m + 1
-
         return -1
