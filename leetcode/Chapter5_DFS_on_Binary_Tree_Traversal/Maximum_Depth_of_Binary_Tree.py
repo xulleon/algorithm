4b9762b54bf98b80f1ff743beb9192a7a32b2fea
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-11 15:23:41 -0800

    Update Maximum_Depth_of_Binary_Tree.py

--- leetcode/Chapter5_DFS_on_Binary_Tree_Traversal/Maximum_Depth_of_Binary_Tree.py
+++ leetcode/Chapter5_DFS_on_Binary_Tree_Traversal/Maximum_Depth_of_Binary_Tree.py
@@ -5,25 +5,26 @@ The reason why it uses preorder  traverse, was because count depth starting from
 
 Traverse means to use a global variable to carry the expected value.  it has to start from a known node which is root. From root, it traverse to its left and right, and each layer.
 
-when it is at root, it needs to calculate its depth by compare the global variable depth with current node depth as a binary may not be balanced.
+when it is at root, it needs to calculate its depth by comparing the global variable depth with the current node depth as a binary may not be balanced.
         self.depth = max(self.depth, cur_depth)
         self.dfs(node.left, cur_depth + 1)
         self.dfs(node.right, cur_depth + 1)
 """
-Divided Conquer
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# Devided & Conquer
+# Devided & Conquer One
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
         return max(self.maxDepth(root.left)+1, self.maxDepth(root.right) + 1)
-#Recursive
+            
+# Devided & Conquer Two
 class Solution:
     depth = 0
     def maxDepth(self, root: Optional[TreeNode]) -> int:
@@ -37,3 +38,24 @@ class Solution:
         self.depth = max(self.depth, cur_depth)
         self.dfs(root.left, cur_depth + 1)
         self.dfs(root.right, cur_depth + 1)
+
+# BFS
+# Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Maximum Depth of Binary Tree.
+# Memory Usage: 18.1 MB, less than 11.90% of Python3 online submissions for Maximum Depth of Binary Tree.
+from collections import deque
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        cnt, queue = 0, deque()
+        queue.append(root)
+        while queue:
+            size = len(queue)
+            cnt += 1
+            for i in range(size):
+                node = queue.popleft()
+                for nbr in [node.left, node.right]:
+                    if nbr:
+                        queue.append(nbr)
+        return cnt
