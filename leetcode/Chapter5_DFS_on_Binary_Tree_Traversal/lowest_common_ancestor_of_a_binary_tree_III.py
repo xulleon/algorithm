eb16669233f0509f2e19d861e967b7fde37a9ae9
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-13 00:44:04 -0800

    add 2nd solution

--- leetcode/Chapter5_DFS_on_Binary_Tree_Traversal/lowest_common_ancestor_of_a_binary_tree_III.py
+++ leetcode/Chapter5_DFS_on_Binary_Tree_Traversal/lowest_common_ancestor_of_a_binary_tree_III.py
@@ -47,3 +47,27 @@ class Solution:
             q = q.parent
 
         return None
+
+# Solution 2
+# Runtime: 84 ms, faster than 5.99% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree III.
+# Memory Usage: 20 MB, less than 52.89% of Python3 online submissions for Lowest Common Ancestor of a Binary
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        if p is None or q is None:
+            return None
+        
+        ps, qs = [], []
+        
+        while p:
+            ps.insert(0, p)
+            p = p.parent
+            
+        while q:
+            qs.insert(0, q)
+            q = q.parent
+
+        cnts = min(len(ps), len(qs))
+        for i in range(cnts):
+            if ps[i] != qs[i]:
+                return ps[i-1]
+        return ps[i]
