6f56de482b1a5fed2c85685aeca32138fccbf415
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-17 00:42:30 -0800

    Add solution 2

--- leetcode/Chapter6_DFS_on_Combination/Word_Break_II.py
+++ leetcode/Chapter6_DFS_on_Combination/Word_Break_II.py
@@ -23,3 +23,34 @@ class Solution:
             subsets.append(prefix)
             self.dfs(s[len(prefix):], wordDict, subsets[:], results)
             subsets.pop()
+
+# Solution 2: a standard DFS template solution.
+# Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Word Break II.
+# Memory Usage: 16.5 MB, less than 95.48% of Python3 online submissions for Word Break II.
+from collections import defaultdict
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        if str == '':
+            return []
+        
+        dp = defaultdict(bool)
+        results = []
+        self.dfs(s, 0, [], wordDict, dp, results)
+
+        return results
+    
+    def dfs(self, s, index, subsets, wordDict, dp, results):
+        if len(s) == len(''.join(subsets)):
+            results.append(' '.join(subsets))
+        
+        for i in range(index, len(s)):
+            prefix = s[index: i + 1]
+            
+            if prefix not in wordDict:
+                continue
+                
+            # strs = str(prefix) if strs == '' else strs + ' ' + str(prefix)
+            subsets.append(prefix)
+            items = self.dfs(s, i + 1, subsets[:], wordDict, dp, results)
+            subsets.pop()
+                                                                     
