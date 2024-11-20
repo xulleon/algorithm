948852862d831cafb5a8c16b18c299f2bc7e278d
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-19 21:54:55 -0800

    Create wildcard_matching.py

---
+++ leetcode/Chapter6_DFS_on_Combination/wildcard_matching.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/wildcard-matching/
+# Runtime: 274 ms, faster than 69.37% of Python3 online submissions for Wildcard Matching.
+# Memory Usage: 25.1 MB, less than 44.26% of Python3 online submissions for Wildcard Matching.
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n, m = len(s), len(p)
+        pattern = []
+        # process p. a****b - a*b
+        si = 0
+        first_start = False
+        while si < m:
+            if p[si] == '*':
+                if not first_start:
+                    
+                    pattern.append(p[si])
+                    first_start = True
+            else:
+                pattern.append(p[si])
+                first_start = False
+            si += 1
+            
+        s = [''] + list(s)
+        pattern = [''] + pattern
+        n, m = len(s), len(pattern)
+        dp = [[False] * (m) for _ in range(n)]
+        
+        dp[0][0] = True
+        if m > 1 and pattern[1] == '*':
+            dp[0][1] = True
+
+        for i in range(1, n):
+            for j in range(1, m):
+                a1=pattern[j]
+                a2=s[i]
+                if pattern[j] == '?' or s[i] == pattern[j]:
+                    dp[i][j] = dp[i-1][j-1]
+                elif pattern[j] == '*':
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
+
+        return dp[n-1][m-1]
