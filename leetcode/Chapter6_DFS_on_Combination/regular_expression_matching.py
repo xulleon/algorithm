7ee69f48cfb83fc5a717b58952c1f58560597135
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-20 01:36:45 -0800

    Update and rename Regular_Expression_Matching.py to regular_expression_matching.py

--- leetcode/Chapter6_DFS_on_Combination/regular_expression_matching.py
+++ leetcode/Chapter6_DFS_on_Combination/regular_expression_matching.py
@@ -44,3 +44,33 @@ class Solution:
             return True
 
         return False
+
+# Dynamic Program
+# https://www.youtube.com/watch?v=l3hda49XcDE&t=2s
+# Runtime: 7 ms, faster than 69.74% of Python3 online submissions for Regular Expression Matching.
+# Memory Usage: 16.7 MB, less than 65.68% of Python3 online submissions for Regular Expression Matching.
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        s = [''] + list(s)
+        p = [''] + list(p)
+        
+        n, m = len(s), len(p)
+        dp = [[False] * m for _ in range(n)]
+        dp[0][0] = True
+        
+        for j in range(1, m):
+            if p[j] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, n):
+            for j in range(1, m):
+                if s[i] == p[j] or p[j] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j] == '*':
+                    dp[i][j] = dp[i][j-2]
+                    if s[i] == p[j-1] or p[j-1] == '.':
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
+                else:
+                    dp[i][j] = False
+                    
+        return dp[n-1][m-1]
