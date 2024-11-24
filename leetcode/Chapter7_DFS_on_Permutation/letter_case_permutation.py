bba1dee91511705e9ba283cf22900bd024a2616a
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-23 22:09:28 -0800

    Create letter_case_permutation.py

---
+++ leetcode/Chapter7_DFS_on_Permutation/letter_case_permutation.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/letter-case-permutation/
+# This is not dfs permutation algorithm!!!
+# Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Letter Case Permutation.
+# Memory Usage: 17.2 MB, less than 97.88% of Python3 online submissions for Letter Case Permutation.
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        output = ['']
+        for char in s:
+            char = char.lower()
+            tmp = []
+            for op in output:
+                tmp.append(op + char)
+                if char.isalpha():
+                    tmp.append(op + char.upper())
+            output = tmp
+                
+        return output
