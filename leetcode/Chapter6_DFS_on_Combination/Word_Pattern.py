29bed169d1094b6a75b52de89cb5163f72c001b5
xulleon <42684153+xulleon@users.noreply.github.com>
2024-11-24 16:43:44 -0800

    Update Word_Pattern.py

--- leetcode/Chapter6_DFS_on_Combination/Word_Pattern.py
+++ leetcode/Chapter6_DFS_on_Combination/Word_Pattern.py
@@ -1,22 +1,25 @@
 # https://leetcode.com/problems/word-pattern/
+# Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Word Pattern.
+# Memory Usage: 16.6 MB, less than 27.27% of Python3 online submissions for Word Pattern.
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
         pattern = list(pattern)
-        s = s.split()
-        if len(pattern) != len(s):
+
+        if len(words) != len(pattern):
             return False
-
-        dp1, dp2 = {}, {}
-        for i in range(len(pattern)):
-            if pattern[i] not in dp1 and s[i] not in dp2:
-                dp1[pattern[i]] = s[i]
-                dp2[s[i]] = pattern[i]
-            elif pattern[i] in dp1 and s[i] in dp2:
-                if dp1[pattern[i]] == s[i] and dp2[s[i]] == pattern[i]:
-                    continue
-                else:
-                    return False
+        
+        word_2_pattern = {}
+        pattern_2_word = {}
+        for p, w in zip(pattern, words):
+            if p not in pattern_2_word:
+                pattern_2_word[p] = w
             else:
-                return False
-
+                if pattern_2_word[p] != w:
+                    return False
+            if w not in word_2_pattern:
+                word_2_pattern[w] = p
+            else:
+                if word_2_pattern[w] != p:
+                    return False
         return True
