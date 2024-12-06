1a47b54f68e36e36b2b9f7525ef0f1f77314e6b8
xulleon <42684153+xulleon@users.noreply.github.com>
2024-12-06 01:12:57 -0800

    add merge sort solution

--- leetcode/Chapter3_Two_Pointer/median_of_two_sorted_arrays.py
+++ leetcode/Chapter3_Two_Pointer/median_of_two_sorted_arrays.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/
+# Solution 1 - heapq
 # Runtime: 4 ms, faster than 35.52% of Python3 online submissions for Median of Two Sorted Arrays.
 # Memory Usage: 16.9 MB, less than 29.62% of Python3 online submissions for Median of Two Sorted Arrays.
 from heapq import heappush, heappop, heapify
@@ -22,3 +23,40 @@ class Solution:
             return (-left[0] + right[0]) / 2
         
         return right[0]
+
+# Solution 2 - mergesort
+# Runtime: 3 ms, faster than 53.22% of Python3 online submissions for Median of Two Sorted Arrays.
+# Memory Usage: 17.5 MB, less than 13.27% of Python3 online submissions for Median of Two Sorted Arrays.
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        '''
+        First use partial merge sort to merge to lists together
+        second, get the median
+        '''
+        n, m = len(nums1), len(nums2)
+        tmp = [0] * (m + n)
+        left_start, tmp_start, right_start = 0, 0, 0
+        while left_start < n and right_start < m:
+            if nums1[left_start] <= nums2[right_start]:
+                tmp[tmp_start] = nums1[left_start]
+                left_start += 1
+            else:
+                tmp[tmp_start] = nums2[right_start]
+                right_start += 1
+            tmp_start += 1
+            
+        while left_start < n:
+            tmp[tmp_start] = nums1[left_start]
+            left_start += 1
+            tmp_start += 1
+        
+        while right_start < m:
+            tmp[tmp_start] = nums2[right_start]
+            right_start += 1
+            tmp_start += 1
+        
+        half = (m + n) // 2
+        if (m + n) %2 == 0:
+            return (tmp[half - 1] + tmp[half]) / 2
+        else:
+            return tmp[half]
