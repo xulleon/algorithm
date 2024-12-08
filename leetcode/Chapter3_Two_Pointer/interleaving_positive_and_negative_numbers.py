ab9e6ceedb0e90c2612eea98a926bfaba547151c
xulleon <42684153+xulleon@users.noreply.github.com>
2024-12-08 01:56:44 -0800

    added solution 2
    recomend to use this one as it is more logic

--- leetcode/Chapter3_Two_Pointer/interleaving_positive_and_negative_numbers.py
+++ leetcode/Chapter3_Two_Pointer/interleaving_positive_and_negative_numbers.py
@@ -63,3 +63,92 @@ class Solution:
             a[p2], a[ptr] = a[ptr], a[p2]
 
         return a
+
+# Solution 2
+# 101 ms time cost· 6.91 MB memory cost· Your submission beats 98.40 % Submissions
+'''
+The key to resolve the problem by
+1)	Use partial quick sort to separate positives and negatives
+2)	 using the following scenario to figure out the starting pos and neg index.
+•	Equal pos and neg [-1,-2,-3,4,5,6], [[-1,-2,-3,-4,5,6,7,8]
+•	More pos [-1,-2,4,5,6], [[-1,-2,-3,5,6,7,8]
+•	More neg [-1,-2,-3,4,5], [[-1,-2,-3,-4,5,6,7]
+
+'''
+from typing import (
+    List,
+)
+class Solution:
+    """
+    @param a: An integer array.
+    @return: nothing
+    """
+    def rerange(self, a: List[int]):
+        # write your code here
+        n = len(a)
+        if n < 3:
+            return a
+
+        # reorg the list by negative first then positive using partial quicksort
+        pl, pr = 0, n - 1
+        while pl <= pr:
+            while pl < pr and a[pl] < 0:
+                pl += 1
+
+            while pl <= pr and a[pr] > 0:
+                pr -= 1
+
+            if pl <= pr:
+                a[pl], a[pr] = a[pr], a[pl]
+                pl += 1
+                pr -= 1
+
+        pos = pl
+        pos_cnt = n - pl
+        neg_cnt = pl
+
+        # same number of positives and negatives
+        if pos_cnt == neg_cnt:
+            # below can be simplified as 
+            # “self.flip(a, 0, pos + (pos_cnt+1)%2)”
+            if pos_cnt % 2 == 0:
+                # start from 1st negative and 2nd positive
+                self.flip(a, 0, pos+1)
+            else:
+                # start from 1st negative and 1st positive
+                self.flip(a, 0, pos)
+        else:
+            if neg_cnt > pos_cnt:
+                # more negatives
+                # more negatives
+                # below can be simplified as 
+                # “self.flip(a, 1, pos + neg_cnt % 2)”
+                if neg_cnt % 2 == 0:
+                    # start from 2nd negative and 1st positive
+                    self.flip(a, 1, pos)
+                else:
+                    # start from 2nd negative and 2nd positive
+                    self.flip(a, 1, pos+1)
+            else:
+                # more positives
+                a.reverse()
+                # the following can be “self.flip(1, n - pos + pos_cnt%2)”
+                if pos_cnt % 2 == 0:
+                    # start from 2nd positive and 1st negative
+                    self.flip(a, 1, n - pos)
+                else:
+                    # start from 2nd positive and 2nd negative
+                    self.flip(a, 1, n-pos + 1)
+
+        return a
+
+    def flip(self, a, start, end):
+        n = len(a)
+        while end < n:
+            a[start], a[end] = a[end], a[start]
+            start += 2
+            end += 2
+
+
+
+
