c1ce6b361682c343e3df00cda33d87187deb9a31
xulleon <42684153+xulleon@users.noreply.github.com>
2024-12-26 00:57:29 -0800

    Update wildcard_matching.py

--- leetcode/Chapter6_DFS_on_Combination/wildcard_matching.py
+++ leetcode/Chapter6_DFS_on_Combination/wildcard_matching.py
@@ -1,43 +1,41 @@
 # https://leetcode.com/problems/wildcard-matching/
 # https://www.youtube.com/watch?v=3ZDZ-N0EPV0
-# Runtime: 274 ms, faster than 69.37% of Python3 online submissions for Wildcard Matching.
-# Memory Usage: 25.1 MB, less than 44.26% of Python3 online submissions for Wildcard Matching.
+# Runtime: 222 ms, faster than 79.63% of Python3 online submissions for Wildcard Matching.
+# Memory Usage: 26.1 MB, less than 35.15% of Python3 online submissions for Wildcard Matching.
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        n, m = len(s), len(p)
-        pattern = []
-        # process p. a****b - a*b
-        si = 0
-        first_start = False
-        while si < m:
-            if p[si] == '*':
-                if not first_start:
-                    
-                    pattern.append(p[si])
-                    first_start = True
+        tmp = []
+        # remove multiple *, i.e. '****', case
+        first_start = True
+        for i in range(len(p)):
+            if p[i] == '*':
+                if first_start:
+                    tmp.append(p[i])
+                    first_start = False
             else:
-                pattern.append(p[si])
-                first_start = False
-            si += 1
-            
+                tmp.append(p[i])
+                first_start = True
+                
         s = [''] + list(s)
-        pattern = [''] + pattern
-        n, m = len(s), len(pattern)
-        dp = [[False] * (m) for _ in range(n)]
+        p = [''] + tmp
+        n, m = len(s), len(p)
+        # Deal with special case
+        if s == '' and (p == '' or p == '*'):
+            return True
         
+        dp = [[False] * m for _ in range(n)]
+        # intialize dp
         dp[0][0] = True
-        if m > 1 and pattern[1] == '*':
+        if m > 1 and p[1] == '*':
             dp[0][1] = True
 
         for i in range(1, n):
             for j in range(1, m):
-                a1=pattern[j]
-                a2=s[i]
-                if pattern[j] == '?' or s[i] == pattern[j]:
-                    # abba vs abb? or abba vs abba, both situations, abb is in both s and p
+                if s[i] == p[j] or p[j] == '?':
                     dp[i][j] = dp[i-1][j-1]
-                elif pattern[j] == '*':
-                    # abba vs abba* or abb vs abb*
-                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
+                elif p[j] == '*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
+                else:
+                    dp[i][j] = False
 
         return dp[n-1][m-1]
